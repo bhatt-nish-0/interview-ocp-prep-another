a9d961d8d98fe89a27e87f3c5d55475e4f7a94ce
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 13:00:46 +0800

    a

---
+++ src/identifiers/Identifiers.java
@@ -0,0 +1,13 @@
+package identifiers;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int lo;
+        int $lo;
+        int _lo;
+        int _3;
+        int this1;
+        //int this;
+        //int super;
+    }
+}
