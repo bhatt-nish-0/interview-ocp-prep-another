f4d252fb9d9937fdc68003cc8d6cc861c4b788bc
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 12:15:01 +0800

    test

---
+++ src/textblock/TextBlock.java
@@ -0,0 +1,16 @@
+package textblock;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        String x = """
+                sss
+                bheem
+                """;
+
+        String x1 = "sss" +
+                "vheem";
+
+        System.out.print(x);
+        System.out.println(x1);
+    }
+}
