9393e5963ae76d61f584f08e3a7d41fe12d0a0a4
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 17:14:37 +0800

    a

---
+++ src/operators/C.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class C {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 23;
+
+        int c = a + b;
+        // a and b are operands
+        // + is operator
+        // = is operator
+
+        int cookies = 4;
+        double reward = 3 + 2 * --cookies;
+
+        System.out.println(reward);
+        System.out.println(cookies);
+    }
+}
