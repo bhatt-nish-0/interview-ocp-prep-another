188e35401018afd54da410391712dc53328ff442
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-09 11:55:20 +0800

    test

--- src/coreapis/string/AdjustingCaseMain.java
+++ src/coreapis/string/AdjustingCaseMain.java
@@ -21,6 +21,27 @@ public class AdjustingCaseMain {
         //equals is ready to receive an object - no need to be
         //string specifically
 
+        String ui = "helloworldfromjimsayshellohi";
 
+        boolean hello = ui.startsWith("hello");
+
+        boolean hello1 = ui.startsWith("hello", 22);
+
+        boolean hello2 = ui.startsWith("hello", 21);
+
+        boolean hello3 = ui.endsWith("hellohi");
+
+        System.out.println(hello);
+        System.out.println(hello1);System.out.println(hello2);
+
+        System.out.println(hello3);
+
+        boolean hello4 = ui.contains("jim");
+
+        boolean hello5 = ui.contains("JIM");
+
+        System.out.println(hello4);
+        System.out.println(hello5);
     }
+
 }
