f1ec3609a9e2db73aa24a3c3a6a5a47cbfc496e1
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-09 12:04:20 +0800

    test

--- src/coreapis/string/ReplacingMain.java
+++ src/coreapis/string/ReplacingMain.java
@@ -10,5 +10,26 @@ public class ReplacingMain {
         r1 = r.replace('l',' ');
         System.out.println(r1);
 
+        String x = "      xxxx           ";
+
+        String x1  = x.trim();
+
+        System.out.println(x1);
+
+        x1 = x.strip();
+        System.out.println(x1);
+
+        x1=x.stripLeading();
+        System.out.println(x1);
+
+        x1=x.stripTrailing();
+        System.out.println(x1);
+
+        String q1 = "\tx";
+        System.out.println(q1);
+
+        System.out.println(q1.length());
+
+        System.out.println(q1.strip().length());
     }
 }
