32697f2f773268260a89aa91fe9d767e13bf1e30
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-09 12:38:51 +0800

    test

--- src/coreapis/string/IdentMain.java
+++ src/coreapis/string/IdentMain.java
@@ -12,5 +12,19 @@ public class IdentMain {
 
         String x2 = x.indent(7);
         System.out.println(x2);
+
+        System.out.println(" ".isEmpty()); // false
+        System.out.println("".isEmpty()); // true
+        System.out.println(" ".isBlank()); // true
+        System.out.println("".isBlank());
+
+        String q = String.format("Hello %s","kate");
+        System.out.println(q);
+
+        q = String.format("hello %d",23);
+        System.out.println(q);
+
+        q = "hi %d %d %d".formatted(2,9,7);
+        System.out.println(q);
     }
 }
